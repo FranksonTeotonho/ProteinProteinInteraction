3bc56979272b57a1e07465d78398646c4cbb092d
frankson <frankson100014@gmail.com>
2017-12-23 22:03:17 -0200

    Matine MMI

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ def main():
     #X3 = AutoEncoders.AutoEnconde(X3,400,200)
     #print(X1)
 
-    # MMI - Human
-    dataset_MMI = pd.read_csv('DatabasesCSV/Human/Human_Database_MMI.csv')
+    # MMI - Matine
+    dataset_MMI = pd.read_csv('DatabasesCSV/Matine/Matine_DataBase_MMI.csv')
     X2 = dataset_MMI.iloc[:, :-1].values
     Y2 = dataset_MMI.iloc[:, 238].values  # BioPython 94, MMI 238
 
